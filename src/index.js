9f123acc3a59ffbe17d43ad6ce68441d5d8ed9c1
FRED NYAKAGWA <fnyakagwa@mo-de.local>
2021-05-19 12:36:44 +0300

    added users route

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ const app = express();
 const PORT = 3000;
 const bodyParser = require('body-parser');
 
+let users = [];
+
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+	res.send(users);
 });
 
 app.listen(PORT, () => {
