a3bf2a2ac65ba3408f2964abb45394df40479897
FRED NYAKAGWA <fnyakagwa@mo-de.local>
2021-05-19 14:26:25 +0300

    Updated post and get a user routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,53 @@
+const { json } = require('express');
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const bodyParser = require('body-parser');
 
 let users = [];
 
+function isID(user) {
+	return user.id === 2;
+}
+
+app.use(express.json());
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
+//Get all users
 app.get('/users', (req, res) => {
 	res.send(users);
+});
+
+//Get a user with specified Id
+app.get('/users/:id', async (req, res) => {
+	const { id } = req.params;
+	const user = await users.find((id) => id === id);
+	try {
+		if (user) {
+			res.send(user);
+		} else {
+			res.json({ msg: 'User not found' });
+		}
+	} catch (err) {
+		res.send(err);
+		console.log(err);
+	}
+});
+
+//Creat users route
+app.post('/users', (req, res) => {
+	const { id } = req.body;
+	try {
+		if (id) {
+			users.push({ id });
+			res.send({ msg: 'User created' });
+		} else {
+			res.json({ msg: 'Invalid Input' });
+		}
+	} catch (err) {
+		res.json(err);
+	}
 });
 
 app.listen(PORT, () => {
